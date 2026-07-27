c2b94f02d249b3494316207c861caa5ee1a3edda
kalpataru-india <reach.kalpataru@gmail.com>
2026-07-27 19:49:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute("href"));
+    if (target) target.scrollIntoView({ behavior: "smooth" });
+  });
+});
